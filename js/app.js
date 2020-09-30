c0e9de9fec5323924d3b68dee154f108e77e0b12
NickLion25 <nickhubanov25@gmail.com>
2020-09-30 15:56:59 +0300

    header

---
+++ js/app.js
@@ -0,0 +1,11 @@
+VANTA.FOG({
+  el: "#header_anim",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  highlightColor: 0xb4fc,
+  midtoneColor: 0xe0e5ed,
+  lowlightColor: 0xf9f9f9,
+  baseColor: 0xe7eff2
+})
